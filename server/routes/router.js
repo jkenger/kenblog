84bcca553d87b0513d513abc8e793474192df8cd
Ken <johnkgervacio@gmail.com>
2022-03-30 13:39:17 +0800

    fetcehd some data

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,16 +7,17 @@ const router = express.Router()
 // Rendering Pages
 router.get('/', services.Home)
 router.get('/home', services.Home)
+
 router.get('/theblog', services.TheBlog)
-
-router.get('/blog', services.Blog)
-
+router.get('/theblog/blogs/:id', services.Blog)
 router.get('/theblog/addblog', services.AddBlog)
+
 router.get('/about', services.About)
 router.get('/contact', services.Contact)
 
-// Controller
+// API
 router.post('/theblog/addblog', controller.CreateBlog)
 router.get('/theblog/blogs', controller.ShowBlogs)
+router.get('/theblog/:id', controller.ShowBlogContent)
 
 module.exports = router
