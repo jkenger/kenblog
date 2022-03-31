af88e891ee63ff5376b2b09573eeb32abcdd580e
Ken <johnkgervacio@gmail.com>
2022-03-31 12:54:44 +0800

    fixed some script bug

--- public/script/script.js
+++
@@ -1,7 +0,0 @@
-var myLink = document.querySelector('ul.links')
-  myLink.addEventListener('click', (e)=>{
-    console.log('clicked: ', e.target)
-    e.preventDefault()
-    window.location.href = `http://localhost:3000/theblog/?filter=${e.target.textContent.replace(/ +/g, "").toLowerCase()}`
-
-})
